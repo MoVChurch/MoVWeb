553e62b6a2747113580c8e6053e60588cbcd08bd
TomThomas <69982935+itomthomas@users.noreply.github.com>
2024-08-24 18:27:10 +0530

    register sw.js in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -275,5 +275,11 @@
       mirror: false
     })
   });
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(reg => console.log('Service Worker registered!', reg))
+      .catch(err => console.error('Service Worker registration failed:', err));
+  }
 
 })()
